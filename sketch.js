a5da358d26666e68a67b8b43c8f809400f26e86f
babyshoes <dianecai@gmail.com>
2018-06-06 11:53:50 -0400

    distorts uniformly

--- sketch.js
+++ sketch.js
@@ -1,10 +1,14 @@
+var watercolor,
+    center;
+
 function setup() {
- createCanvas(400, 400);
+    createCanvas(400, 400);
+    center = new Point(width/2, height/2);
+    watercolor = new Watercolor(center.x, center.y, 80, 10);
 }
 
 function draw() {
-    background('rgba(255, 218, 119, 0.5)');
-    watercolor = new Watercolor(width/2, height/2, 80, 8);
+    background('rgba(255, 218, 119, .2)');
     watercolor.paint();
 }
 
@@ -19,21 +23,36 @@ class Edge {
     constructor(points){
         this.first = points[0];
         this.second = points[1];
+        // debugger
+        this.length = this.findLength();
+        this.midpoint = this.getMidpoint();
+        this.orthogonalAngle = this.getAngleFromCenter();
     }
 
-    findLength(vertices) {
-        return Math.pow(Math.pow(Math.abs(this.first.x - this.second.x), 2) + Math.pow(Math.abs(this.first.x - this.second.x), 2), 0.5);
+    findLength() {
+        let a = this.first.x - this.second.x;
+        let b = this.first.y - this.second.y;
+        return Math.pow(Math.pow(a, 2) + Math.pow(b, 2), 0.5);
     }
 
-    getMidpoint(vertices) {
+    getMidpoint() {
         return new Point((this.first.x + this.second.x)/2, (this.first.y + this.second.y)/2)
+    }
+
+    getAngleRadians() {
+        return Math.atan2(this.second.y - this.first.y, this.second.x - this.first.x);
+    }
+
+    getAngleFromCenter() {
+        return Math.atan2(this.midpoint.y - center.y, this.midpoint.x - center.x);
     }
 }
 
 class Watercolor {
     constructor(x, y, radius, npoints) {
         this.vertices = [];
-        this.makePolygon(x, y, radius, npoints);
+        this.vertices = this.makePolygon(x, y, radius, npoints);
+        this.vertices = this.distort();
     }
 
     paint() {
@@ -44,30 +63,70 @@ class Watercolor {
         endShape();
     }
 
+    projectX(x, angle, r) {
+        return x + cos(angle) * r;
+    }
+
+    projectY(y, angle, r) {
+        return y + sin(angle) * r;
+    }
+
     makePolygon(x, y, radius, npoints) {
         var angle,
             sx,
             sy
+        var vertices = []
         angle = TWO_PI / npoints;
-        for (let i=0; i < TWO_PI; i+=angle) {
+        for ( let i = 0; i < TWO_PI; i += angle ) {
             sx = x + cos(i) * radius;
             sy = y + sin(i) * radius;
-            this.vertices.push(new Point(sx, sy));
+            vertices.push( new Point(sx, sy) );
         }
+
+        return vertices;
     }
 
-    distort(neighboringVertices) {
+    distortEdge(edge) {
         var length,
             angle,
             magnitude,
-            vertex
-        length = getEdge(neighboringVertices);
+            vertex,
+            newX,
+            newY
+        // edge = new Edge(neighboringVertices);
+        length = edge.length;
         // pick starting point on edge, vary a lil
-        vertex = getMidpoint(vertices);
+        vertex = edge.midpoint;
         // pick angle for breaking edge
-        angle = randomGaussian(HALF_PI, HALF_PI * .1)
+        angle = edge.orthogonalAngle;
+        // + randomGaussian(HALF_PI, HALF_PI * .1)
         // pick magnitude of distortion, vary w/ length
-
+        // magnitude = randomGaussian(1, .2) * length
+        magnitude = length
+        // magnitude = magnitude < 0 ? 0 : magnitude
         // get new vertex
+        newX = this.projectX(vertex.x, angle, magnitude);
+        newY = this.projectY(vertex.y, angle, magnitude);
+
+        return new Point(newX, newY);
+    }
+
+    distort() {
+        var edge,
+            ptA,
+            ptB,
+            newVertex;
+        var newVertices = []
+        for ( let i=0; i<this.vertices.length; i++ ) {
+            ptA = this.vertices[i];
+            // wrap around to first vertex
+            ptB = i == this.vertices.length-1 ? this.vertices[0] : this.vertices[i+1];
+
+            edge = new Edge( [ptA, ptB] );
+            newVertex = this.distortEdge(edge);
+            newVertices.push(this.vertices[i],newVertex);
+        }
+        return newVertices;
+        
     }
 }
